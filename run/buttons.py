f580dfc2bf513287ccd510f7ab8f7b4706f08877
Kaliboy002 <146410941+Kaliboy002@users.noreply.github.com>
2024-12-21 20:11:18 +0430

    Update buttons.py

--- run/buttons.py
+++ run/buttons.py
@@ -1,14 +1,11 @@
 from run import Button
 
+from run import Button
+
 
 class Buttons:
-    source_code_button = [
-        Button.url("Source Code", url="https://github.com/AdibNikjou/MusicDownloader-Telegram-Bot")]
-
     main_menu_buttons = [
-        [Button.inline("Instructions", b"instructions"), Button.inline("Settings", b"setting")],
-        source_code_button,
-        [Button.url("Contact Creator", url="telegram.me/adibnikjou")],
+        [Button.inline("Instructions", b"instructions")],
     ]
 
     back_button = Button.inline("<< Back To Main Menu", b"back")
