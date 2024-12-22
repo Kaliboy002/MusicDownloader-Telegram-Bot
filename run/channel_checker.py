92dca238c1acb503272dacd48d2c6ee62aace7d7
Kaliboy002 <146410941+Kaliboy002@users.noreply.github.com>
2024-12-22 19:32:18 +0430

    Update channel_checker.py

--- run/channel_checker.py
+++ run/channel_checker.py
@@ -11,27 +11,23 @@ async def is_user_in_channel(user_id, channel_usernames=None):
     channels_user_is_not_in = []
 
     for channel_username in channel_usernames:
-        channel = await BotState.BOT_CLIENT.get_entity(channel_username)
-        offset = 0
-        while True:
-            try:
-                participants = await BotState.BOT_CLIENT(GetParticipantsRequest(
-                    channel,
-                    ChannelParticipantsSearch(''),  # Search query, empty for all participants
-                    offset=offset,  # Providing the offset
-                    limit=10 ** 9,  # Adjust the limit as needed
-                    hash=0
-                ))
-            except ChatAdminRequiredError:
-                print(f"ChatAdminRequiredError: Bot does not have admin privileges in {channel_username}.")
-                break
-            if not participants.users:
-                break  # No more participants to fetch
+        try:
+            channel = await BotState.BOT_CLIENT.get_entity(channel_username)
+            participants = await BotState.BOT_CLIENT(GetParticipantsRequest(
+                channel,
+                ChannelParticipantsSearch(''),  # Search query, empty for all participants
+                offset=0,  # Offset for fetching participants
+                limit=200,  # Fetch participants in batches of 200
+                hash=0
+            ))
             if not any(participant.id == user_id for participant in participants.users):
                 channels_user_is_not_in.append(channel_username)
-                break  # User found, no need to check other channels
-
-            offset += len(participants.users)  # Increment offset for the next batch
+        except ChatAdminRequiredError:
+            print(f"ChatAdminRequiredError: Bot does not have admin privileges in {channel_username}.")
+            continue
+        except Exception as e:
+            print(f"Unexpected error while checking channel {channel_username}: {e}")
+            continue
     return channels_user_is_not_in
 
 
@@ -55,28 +51,28 @@ async def respond_based_on_channel_membership(event, message_if_in_channels: str
     user_id = event.sender_id
     buttons_if_in_channel = buttons
 
-    channels_user_is_not_in = await is_user_in_channel(
-        user_id) if channels_user_is_not_in is None else channels_user_is_not_in
+    if channels_user_is_not_in is None:
+        channels_user_is_not_in = await is_user_in_channel(user_id)
 
-    if channels_user_is_not_in != [] and (user_id not in BotState.ADMIN_USER_IDS):
+    if channels_user_is_not_in and (user_id not in BotState.ADMIN_USER_IDS):
         join_channel_buttons = [[join_channel_button(channel)] for channel in channels_user_is_not_in]
         join_channel_buttons.append([optional_redirect_button()])  # Add the optional button here
         join_channel_buttons.append(Buttons.continue_button)
         await BotMessageHandler.send_message(event,
                                              f"""Hey {sender_name}!👋 \n{BotMessageHandler.JOIN_CHANNEL_MESSAGE}""",
                                              buttons=join_channel_buttons)
-    elif message_if_in_channels is not None or (user_id in BotState.ADMIN_USER_IDS):
-        await BotMessageHandler.send_message(event, f"""{message_if_in_channels}""",
-                                             buttons=buttons_if_in_channel)
+    else:
+        await BotMessageHandler.send_message(event, message_if_in_channels, buttons=buttons_if_in_channel)
 
 
 async def handle_continue_in_membership_message(event):
     sender_name = event.sender.first_name
     user_id = event.sender_id
     channels_user_is_not_in = await is_user_in_channel(user_id)
-    if channels_user_is_not_in != []:
+
+    if channels_user_is_not_in:
         join_channel_buttons = [[join_channel_button(channel)] for channel in channels_user_is_not_in]
-        join_channel_buttons.append([optional_redirect_button()])  # Add the optional button here
+        join_channel_buttons.append([optional_redirect_button()])
         join_channel_buttons.append(Buttons.continue_button)
         await BotMessageHandler.edit_message(event,
                                              f"""Hey {sender_name}!👋 \n{BotMessageHandler.JOIN_CHANNEL_MESSAGE}""",
